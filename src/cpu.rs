7dcaae796f66958cf4c7af9bfa2c6279a1da3fb7
Paul Ramirez <purpaul777@gmail.com>
2022-05-14 15:20:57 -0400

    Added tests for LDA and implemented TAX

--- src/cpu.rs
+++ src/cpu.rs
@@ -4,6 +4,7 @@ pub fn main(){
 // CPU Registers:
 pub struct CPU {
     pub register_a: u8, // Accumulator
+    pub register_x: u8,
     // Represents status, each bit is a flag (in order below):
     // Negative, Overflow, ____ (unused, always set), Break,
     // Decimal, Interrupt Disable, Zero, Carry 
@@ -16,6 +17,7 @@ impl CPU {
     pub fn new() -> Self{
         CPU {
             register_a: 0,
+            register_x: 0,
             status: 0,
             program_counter: 0
         }    
@@ -60,8 +62,53 @@ impl CPU {
                 0x00 => {// BRK: Break
                     return;
                 }
+                0xAA => {// TAX: Transfer Accumulator to X
+                    self.register_x = self.register_a; // Copy Accumulator onto Register X
+
+                    // Set Zero flag if needed
+                    if self.register_x == 0 {
+                        self.status = self.status | 0b0000_0010;
+                    } else {
+                        self.status = self.status & 0b1111_1101;
+                    }
+
+                    // Set Negative flag if needed
+                    if self.register_x & 0b1000_0000 != 0 {
+                        self.status = self.status | 0b1000_0000;
+                    } else {
+                        self.status = self.status & 0b0111_1111;
+                    }
+                }
                 _ => todo!("")
             }
         } // REPEAT
+    }
+}
+
+#[cfg(test)]
+mod test { 
+    use super::*; // all functions in parent
+
+    #[test]
+    fn test_0xa9_lda_immediate_load_date(){
+        let mut cpu: CPU = CPU::new();
+        cpu.interpret(vec![0xa9, 0x05, 0x00]); // LDA 0X05 BRK
+        assert_eq!(cpu.register_a, 0x05); // Value loaded onto Accumulator
+        assert!(cpu.status & 0b0000_0010 == 0); // Zero flag set
+        assert!(cpu.status & 0b1000_0000 == 0); // Negative flag not set
+    }
+    #[test]
+    fn test_0xa9_lda_zero_flag(){
+        let mut cpu: CPU = CPU::new();
+        cpu.interpret(vec![0xa9, 0x00, 0x00]);// LDA 0X00 BRK
+        assert_eq!(cpu.register_a, 0x00); // Value loaded onto Accumulator
+        assert!(cpu.status & 0b0000_0010 == 0b0000_0010); // Zero flag set
+    }
+    #[test]
+    fn test_0xa9_lda_negative_flag(){
+        let mut cpu: CPU = CPU::new();
+        cpu.interpret(vec![0xa9, 0b1000_0001, 0x00]); // LDA 0X41 BRK
+        assert_eq!(cpu.register_a, 0b1000_0001); // Value loaded onto Accumulator
+        assert!(cpu.status & 0b1000_0000 == 0b1000_0000); // Negative flag set
     }
 }
