a300bf82df984fcb3f9e9c2a0872fa1d7d1604fd
Paul Ramirez <purpaul777@gmail.com>
2022-05-15 14:01:25 -0400

    Reorganized code and added 151 instructions to map

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 pub mod cpu;
 pub mod opcodes;
-fn main(){
-    
+fn main() {
     println!("Hello, world!");
 }
--- src/opcodes.rs
+++ src/opcodes.rs
@@ -1,20 +1,26 @@
 use crate::cpu::AddressingMode;
 pub struct OpCode {
-    instruction: u8,
-    name: String,
-    num_bytes: u8,
-    num_cycles: u8,
-    mode: AddressingMode
+    pub instruction: u8,
+    pub name: String,
+    pub num_bytes: u8,
+    pub num_cycles: u8,
+    pub mode: AddressingMode,
 }
 
 impl OpCode {
-    pub fn new() -> Self{
+    pub fn new(
+        instruction: u8,
+        name: String,
+        num_bytes: u8,
+        num_cycles: u8,
+        mode: AddressingMode,
+    ) -> Self {
         OpCode {
-            instruction:0,
-            name:String::new(),
-            num_bytes:0,
-            num_cycles: 0,
-            mode: AddressingMode::NoneAddressing
+            instruction,
+            name,
+            num_bytes,
+            num_cycles,
+            mode,
         }
     }
 }
