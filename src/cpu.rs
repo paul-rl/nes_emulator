cc06be48475d3593f61bb5b40e0106b57e6eb280
Paul Ramirez <purpaul777@gmail.com>
2022-05-14 20:59:02 -0400

    Added LDA and (some) STA addressing modes to cpu cycle

--- src/cpu.rs
+++ src/cpu.rs
@@ -12,7 +12,7 @@ pub enum AddressingMode {
     Absolute_Y,
     Indirect_X,
     Indirect_Y,
-    NoneAddressing
+    NoneAddressing,
 }
 // CPU Registers:
 pub struct CPU {
@@ -25,7 +25,7 @@ pub struct CPU {
     // E.g. if value is 0100011: Zero, Overflow, Negative flags all set
     pub status: u8,
     pub program_counter: u16, // Holds address for next instruction
-    pub memory: [u8; 0xFFFF] // 64 KiB array simulating memory
+    pub memory: [u8; 0xFFFF], // 64 KiB array simulating memory
 }
 
 impl CPU {
@@ -40,8 +40,8 @@ impl CPU {
         }
     }
 
-    pub fn get_operand_address(&self, mode: &AddressingMode) -> u16{
-        match mode{
+    pub fn get_operand_address(&self, mode: &AddressingMode) -> u16 {
+        match mode {
             AddressingMode::Immediate => self.program_counter,
             AddressingMode::ZeroPage => self.mem_read(self.program_counter) as u16,
             AddressingMode::ZeroPage_X => {
@@ -53,7 +53,7 @@ impl CPU {
                 let pos: u8 = self.mem_read(self.program_counter);
                 let addr: u16 = pos.wrapping_add(self.register_y) as u16;
                 addr
-            },
+            }
             AddressingMode::Absolute => self.mem_read_u16(self.program_counter),
             AddressingMode::Absolute_X => {
                 let pos: u16 = self.mem_read_u16(self.program_counter);
@@ -86,7 +86,7 @@ impl CPU {
         }
     }
     // Reads from given address in memory
-    pub fn mem_read(&self, addr: u16) -> u8{
+    pub fn mem_read(&self, addr: u16) -> u8 {
         self.memory[addr as usize]
     }
 
@@ -98,14 +98,14 @@ impl CPU {
     // NES uses little endian for u16: 0x8000 written as 00 80 (L to R)
     // When reading u16, read two consecutive registers, and switch their order around to get
     // the stored value
-    pub fn mem_read_u16(&self, addr: u16) -> u16{
+    pub fn mem_read_u16(&self, addr: u16) -> u16 {
         let lo: u16 = self.mem_read(addr) as u16;
         let hi: u16 = self.mem_read(addr + 1) as u16;
         (hi << 8) | lo
     }
 
     // When writing u16, take upper and lower 8 bits and store them in reverse order.
-    pub fn mem_write_u16(&mut self, addr: u16, data: u16){
+    pub fn mem_write_u16(&mut self, addr: u16, data: u16) {
         let hi: u8 = (data >> 8) as u8; // Extract last 8 bits
         let lo: u8 = (data & 0xff) as u8; // Extract first 8 bits
         self.mem_write(addr, lo);
@@ -113,20 +113,20 @@ impl CPU {
     }
 
     // Loads program into memory, resets registers, then runs the program
-    pub fn load_and_run(&mut self, program: Vec<u8>){
+    pub fn load_and_run(&mut self, program: Vec<u8>) {
         self.load(program);
         self.reset();
         self.run();
     }
 
     // Loads program into memory and sets PC to value found in 0xFFFC
-    pub fn load(&mut self, program: Vec<u8>){
-        self.memory[0x8000 .. (0x8000 + program.len())].copy_from_slice(&program[..]); // Load program into memory
+    pub fn load(&mut self, program: Vec<u8>) {
+        self.memory[0x8000..(0x8000 + program.len())].copy_from_slice(&program[..]); // Load program into memory
         self.mem_write_u16(0xFFFC, 0x8000);
     }
 
     // Restore set of all registers and initialize PC to 2 byte value stored in 0xFFFC
-    pub fn reset(&mut self){
+    pub fn reset(&mut self) {
         self.register_a = 0;
         self.register_x = 0;
         self.status = 0;
@@ -142,17 +142,68 @@ impl CPU {
         // Execute
         // Repeat
         '_cpu_cycle: loop {
-            
             let opcode: u8 = self.mem_read(self.program_counter); // Fetch
             self.program_counter += 1; // PC UPDATE
-            
+
             match opcode {
                 // DECODE, then on match EXECUTE
                 0xA9 => {
-                    //LDA: Load Accumulator with Memory
-                    let param = self.mem_read(self.program_counter);
+                    //LDA: Load Accumulator with Memory, Immediate
+                    self.lda(&AddressingMode::Immediate);
                     self.program_counter += 1;
-                    self.lda(param);
+                }
+
+                0xAD => {
+                    //LDA: Load Accumulator with Memory, Absolute
+                    self.lda(&AddressingMode::Absolute);
+                    self.program_counter += 1;
+                }
+
+                0xBD => {
+                    //LDA: Load Accumulator with Memory, X-Indexed Absolute
+                    self.lda(&AddressingMode::Absolute_X);
+                    self.program_counter += 1;
+                }
+
+                0xB9 => {
+                    //LDA: Load Accumulator with Memory, Y-Indexed Absolute
+                    self.lda(&AddressingMode::Absolute_Y);
+                    self.program_counter += 1;
+                }
+
+                0xA5 => {
+                    //LDA: Load Accumulator with Memory, Zero Page
+                    self.lda(&AddressingMode::ZeroPage);
+                    self.program_counter += 1;
+                }
+
+                0xB5 => {
+                    //LDA: Load Accumulator with Memory, Zero Page X
+                    self.lda(&AddressingMode::ZeroPage_X);
+                    self.program_counter += 1;
+                }
+
+                0xA1 => {
+                    //LDA: Load Accumulator with Memory, Indirect X
+                    self.lda(&AddressingMode::Indirect_X);
+                    self.program_counter += 1;
+                }
+
+                0xB1 => {
+                    //LDA: Load Accumulator with Memory, Indirect Y
+                    self.lda(&AddressingMode::Indirect_Y);
+                    self.program_counter += 1;
+                }
+
+                0x8D => {}
+                0x85 => {
+                    self.sta(&AddressingMode::ZeroPage);
+                    self.program_counter += 1;
+                }
+
+                0x95 => {
+                    self.sta(&AddressingMode::ZeroPage_X);
+                    self.program_counter += 1;
                 }
 
                 0x00 => return, // BRK: Break
@@ -165,9 +216,14 @@ impl CPU {
         } // REPEAT
     }
 
-    fn lda(&mut self, value: u8) {
-        self.register_a = value;
+    fn lda(&mut self, mode: &AddressingMode) {
+        self.register_a = self.mem_read(self.get_operand_address(mode));
         self.update_zero_and_negative_flags(self.register_a);
+    }
+
+    // STA: Store Accumulator in Memory
+    fn sta(&mut self, mode: &AddressingMode) {
+        self.mem_write(self.get_operand_address(mode), self.register_a);
     }
 
     // TAX: Transfer Accumulator to X
@@ -181,7 +237,7 @@ impl CPU {
         print!("Pre {}", self.register_x);
         self.register_x = self.register_x.wrapping_add(1);
         print!("Post {}", self.register_x);
-        
+
         self.update_zero_and_negative_flags(self.register_x);
     }
 
@@ -201,7 +257,6 @@ impl CPU {
         }
     }
 }
-
 
 #[cfg(test)]
 mod test {
@@ -238,7 +293,7 @@ mod test {
         cpu.load(vec![0xAA, 0x00]); // TAX BRK
         cpu.program_counter = cpu.mem_read_u16(0xFFFC);
         cpu.run();
-        
+
         assert_eq!(cpu.register_x, 10); // Value loaded onto Accumulator
         assert!(cpu.status & 0b0000_0010 == 0); // Zero flag not set
         assert!(cpu.status & 0b1000_0000 == 0); // Negative flag not set
@@ -282,29 +337,36 @@ mod test {
         assert_eq!(cpu.register_x, 1);
     }
     #[test]
-    fn test_program_load(){
+    fn test_program_load() {
         let mut cpu: CPU = CPU::new();
         let program: Vec<u8> = vec![0xe8, 0xe8, 0xe8, 0xaa, 0x00];
         cpu.load(program.clone());
         let mut i: u16 = 0;
-        for data in program.iter(){
+        for data in program.iter() {
             let data_in_memory: u8 = cpu.memory[(0x8000 + i) as usize];
-            i+=1;
+            i += 1;
             assert_eq!(data_in_memory, *data);
         }
     }
 
     #[test]
-    fn test_mem_read(){
+    fn test_mem_read() {
         let mut cpu: CPU = CPU::new();
         let program: Vec<u8> = vec![0xe8, 0xe8, 0xe8, 0xaa, 0x00];
         cpu.load(program.clone());
         let mut i: u16 = 0;
-        for data in program.iter(){
+        for data in program.iter() {
             let data_in_memory: u8 = cpu.mem_read(0x8000 + i);
-            i+=1;
+            i += 1;
             assert_eq!(data_in_memory, *data);
         }
     }
-    
+
+    #[test]
+    fn test_lda_from_memory() {
+        let mut cpu: CPU = CPU::new();
+        cpu.mem_write(0x10, 0x55);
+        cpu.load_and_run(vec![0xa5, 0x10, 0x00]);
+        assert_eq!(cpu.register_a, 0x55);
+    }
 }
