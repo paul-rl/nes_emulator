5f24a9731be4da1d33e934fe5d5cecf9d0494f6a
Paul Ramirez <purpaul777@gmail.com>
2022-05-27 10:39:13 -0400

    Added user input handling, writing & reading to screen

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 pub mod cpu;
 pub mod opcodes;
 use cpu::*;
+use rand::Rng;
 use sdl2::VideoSubsystem;
 use sdl2::event::Event;
 use sdl2::EventPump;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
 use sdl2::pixels::PixelFormatEnum;
-use sdl2::sys::Window;
-use sdl2::video::WindowBuilder;
+
 
 fn main() {
     // init sdl2, use unwrap because it's outer layer
@@ -29,6 +29,8 @@ fn main() {
     let creator = canvas.texture_creator();
     // Make our 32x32 texture, each represented by 3 bytes (R, G, B)
     let mut texture = creator.create_texture_target(PixelFormatEnum::RGB24, 32, 32).unwrap();
+
+
 
     let _game_code = vec![
         0x20, 0x06, 0x06, 0x20, 0x38, 0x06, 0x20, 0x0d, 0x06, 0x20, 0x2a, 0x06, 0x60,//
@@ -76,14 +78,81 @@ fn main() {
     let mut cpu = CPU::new();
     cpu.load(_game_code);
     cpu.reset();
+    
+    let mut screen_state = [0 as u8; 32 * 3 * 32];
+    let mut rng = rand::thread_rng();
 
     // run the game cycle
     cpu.run_with_callback( move |cpu| {
-        // TODO:
         // read user input and write it to mem[0xFF]
+        handle_user_input(cpu, &mut event_pump);
+        cpu.mem_write(0xfe, rng.gen_range(1, 16));
+
+        if read_screen_state(cpu, &mut  screen_state) {
+            texture.update(None, &screen_state, 32 * 3).unwrap();
+            canvas.present();
+        }
+
+        ::std::thread::sleep(std::time::Duration::new(0, 70_000));
         // update mem[0xFE] with new Random Number
         // read mem mapped screen state
         // render screen state
 
     });
 }
+
+fn handle_user_input(cpu: &mut CPU, event_pump: &mut EventPump) {
+    for event in event_pump.poll_iter() {
+        match event {
+            Event::Quit { .. } | Event::KeyDown { keycode: Some(Keycode::Escape), ..} => {
+                std::process::exit(0)
+            },
+            Event::KeyDown {keycode: Some(Keycode::W), ..} => {
+                cpu.mem_write(0xff, 0x77);
+            }
+            Event::KeyDown {keycode: Some(Keycode::S), ..} => {
+                cpu.mem_write(0xff, 0x73);
+            }
+            Event::KeyDown {keycode: Some(Keycode::A), ..} => {
+                cpu.mem_write(0xff, 0x61);
+            }
+            Event::KeyDown {keycode: Some(Keycode::D), ..} => {
+                cpu.mem_write(0xff, 0x64);
+            }
+            _ => {/* Do nothing */}
+        }
+    }
+}
+
+fn color(byte: u8) -> Color {
+    match byte {
+       0 => sdl2::pixels::Color::BLACK,
+       1 => sdl2::pixels::Color::WHITE,
+       2 | 9 => sdl2::pixels::Color::GREY,
+       3 | 10 => sdl2::pixels::Color::RED,
+       4 | 11 => sdl2::pixels::Color::GREEN,
+       5 | 12 => sdl2::pixels::Color::BLUE,
+       6 | 13 => sdl2::pixels::Color::MAGENTA,
+       7 | 14 => sdl2::pixels::Color::YELLOW,
+       _ => sdl2::pixels::Color::CYAN,
+    }
+}
+
+fn read_screen_state(cpu: &CPU, frame: &mut [u8; 32 * 3 * 32]) -> bool {
+   let mut frame_idx = 0;
+   let mut update = false;
+   for i in 0x0200..0x600 {
+       let color_idx = cpu.mem_read(i as u16);
+       let (b1, b2, b3) = color(color_idx).rgb();
+       // Only update pixel if it is changed.
+       if frame[frame_idx] != b1 || frame[frame_idx + 1] != b2 || frame[frame_idx + 2] != b3 {
+           frame[frame_idx] = b1;
+           frame[frame_idx + 1] = b2;
+           frame[frame_idx + 2] = b3;
+           update  = true;
+       }
+       frame_idx += 3;
+   }
+   update
+}
+
